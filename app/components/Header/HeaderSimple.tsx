cfa22e600dabe0d4eb65cad63d8028f62f10acaf
tamadam <tamadam9@gmail.com>
2025-03-30 15:30:00 +0200

    Add SimpleHeader basic layout

--- app/components/Header/HeaderSimple.tsx
+++ app/components/Header/HeaderSimple.tsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import styles from "./HeaderSimple.module.scss";
+import {
+  F1_LOGO_ORIGINAL_HEIGHT,
+  F1_LOGO_ORIGINAL_WIDTH,
+} from "@/app/constants";
+
 const HeaderSimple = () => {
-  return <header>HeaderSimple</header>;
+  return (
+    <header>
+      <div className={styles.headerWrapper}>
+        <div className={styles.headerItem}>
+          <Image
+            src="/f1DatasetLogo.svg"
+            width={F1_LOGO_ORIGINAL_WIDTH / 8}
+            height={F1_LOGO_ORIGINAL_HEIGHT / 8}
+            alt="F1 Dataset Logo"
+          />
+        </div>
+        <div className={styles.headerItem}>
+          <div>Language Selector</div>
+          <div>Explore Button</div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default HeaderSimple;

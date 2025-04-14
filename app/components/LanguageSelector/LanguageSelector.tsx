4a04907f045d8e68565d63ef90dafcce59799688
tamadam <tamadam9@gmail.com>
2025-04-14 15:18:59 +0200

    Modify Language Selector to use Container queries

--- app/components/LanguageSelector/LanguageSelector.tsx
+++ app/components/LanguageSelector/LanguageSelector.tsx
@@ -5,20 +5,22 @@ import styles from "./LanguageSelector.module.scss";
 
 const LanguageSelector = () => {
   return (
-    <div className={styles.languageSelectorWrapper}>
-      <div className={styles.languageSelectorIcon} aria-hidden>
-        <Globe width={16} height={16} focusable={false} />
-      </div>
-      <select
-        className={styles.languageSelector}
-        name="language-select"
-        aria-label="Choose language"
-      >
-        <option value="hu">Magyar</option>
-        <option value="en">English</option>
-      </select>
-      <div className={styles.languageSelectorIcon} aria-hidden>
-        <ArrowDown width={16} height={16} focusable={false} />
+    <div className={styles.languageSelectorOuterWrapper}>
+      <div className={styles.languageSelectorWrapper}>
+        <div className={styles.languageSelectorIcon} aria-hidden>
+          <Globe width={16} height={16} focusable={false} />
+        </div>
+        <select
+          className={styles.languageSelector}
+          name="language-select"
+          aria-label="Choose language"
+        >
+          <option value="hu">Magyar</option>
+          <option value="en">English</option>
+        </select>
+        <div className={styles.languageSelectorIcon} aria-hidden>
+          <ArrowDown width={16} height={16} focusable={false} />
+        </div>
       </div>
     </div>
   );

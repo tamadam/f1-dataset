ac0b2b52a8c3965ed3557800f652deb13e84b642
tamadam <tamadam9@gmail.com>
2025-08-24 13:49:49 +0200

    Disable cache skip for races & add missing json

--- app/lib/api/getAllRaces.ts
+++ app/lib/api/getAllRaces.ts
@@ -8,20 +8,21 @@ export const getAllRaces = async (year: string): Promise<RawRaces | null> => {
     const endpoint = `${F1_API_BASE_URL}/${year}/races`;
     const cacheSubFolder = ["races"];
     const cacheKey = generateCacheKey("races", year);
-    const currentYear = new Date().getFullYear().toString();
-    const isCurrentSeason = year === currentYear;
 
-    const skipCustomCache = process.env.NODE_ENV !== 'production' || isCurrentSeason;
-    
+    const skipCustomCache = false;
+
     return await fetchWithCacheAndRateLimit<RawRaces>(
       endpoint,
       cacheSubFolder,
       cacheKey,
       skipCustomCache,
-      (data) => Boolean(data?.MRData?.RaceTable?.Races),
+      (data) => Boolean(data?.MRData?.RaceTable?.Races)
     );
   } catch (error) {
-    throw new Error(`Failed to fetch all races for ${year}: ${error instanceof Error ? error.message : "Unknown error"}`);
+    throw new Error(
+      `Failed to fetch all races for ${year}: ${
+        error instanceof Error ? error.message : "Unknown error"
+      }`
+    );
   }
 };
-

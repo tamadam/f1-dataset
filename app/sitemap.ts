bed17ef59795ecec940204b31a9db6c7aa9ad88a
tamadam <tamadam9@gmail.com>
2025-10-11 11:54:02 +0200

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,73 @@
+import type { MetadataRoute } from "next";
+import { getAllF1Years } from "./lib/year-utils";
+import { routing } from "@/i18n/routing";
+import { CATEGORIES } from "./constants";
+import { getAllDrivers } from "./lib/api/getAllDrivers";
+import { getAllConstructors } from "./lib/api/getAllConstructors";
+import { getAllRaces } from "./lib/api/getAllRaces";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://f1-dataset.vercel.app";
+  const years = getAllF1Years().map(String);
+  const routes: MetadataRoute.Sitemap = [];
+
+  for (const locale of routing.locales) {
+    // landing page
+    routes.push({
+      url: `${baseUrl}/${locale}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    });
+
+    for (const year of years) {
+      for (const category of CATEGORIES) {
+        // category pages
+        routes.push({
+          url: `${baseUrl}/${locale}/results/${year}/${category}`,
+          lastModified: new Date(),
+          changeFrequency: "weekly",
+          priority: 1,
+        });
+
+        if (category === "drivers") {
+          const drivers = await getAllDrivers(year);
+          for (const driver of drivers) {
+            // driver specific pages
+            routes.push({
+              url: `${baseUrl}/${locale}/results/${year}/${category}/${driver.driverId}`,
+              lastModified: new Date(),
+              changeFrequency: "weekly",
+              priority: 0.8,
+            });
+          }
+        } else if (category === "constructors") {
+          const constructors = await getAllConstructors(year);
+          for (const constructor of constructors) {
+            // constructor specific pages
+            routes.push({
+              url: `${baseUrl}/${locale}/results/${year}/${category}/${constructor.constructorId}`,
+              lastModified: new Date(),
+              changeFrequency: "weekly",
+              priority: 0.8,
+            });
+          }
+        } else if (category === "races") {
+          const racesData = await getAllRaces(year);
+          const races = racesData?.MRData.RaceTable.Races ?? [];
+          for (const race of races) {
+            // race specific pages
+            routes.push({
+              url: `${baseUrl}/${locale}/results/${year}/${category}/${race.Circuit.circuitId}`,
+              lastModified: new Date(),
+              changeFrequency: "weekly",
+              priority: 0.8,
+            });
+          }
+        }
+      }
+    }
+  }
+
+  return routes;
+}

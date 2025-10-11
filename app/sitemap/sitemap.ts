f2d0cfc9448ffe78e3e9c319765afab736c30db9
tamadam <tamadam9@gmail.com>
2025-10-11 20:15:34 +0200

    remove subcategory pages from sitemap

--- app/sitemap/sitemap.ts
+++ app/sitemap/sitemap.ts
@@ -2,9 +2,6 @@ import type { MetadataRoute } from "next";
 import { getAllF1Years } from "@/app/lib/year-utils";
 import { routing } from "@/i18n/routing";
 import { APP_BASE_URL, CATEGORIES } from "@/app/constants";
-import { getAllDrivers } from "@/app/lib/api/getAllDrivers";
-import { getAllConstructors } from "@/app/lib/api/getAllConstructors";
-import { getAllRaces } from "@/app/lib/api/getAllRaces";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const years = getAllF1Years().map(String);
@@ -28,42 +25,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
           changeFrequency: "weekly",
           priority: 1,
         });
-
-        if (category === "drivers") {
-          const drivers = await getAllDrivers(year);
-          for (const driver of drivers) {
-            // driver specific pages
-            routes.push({
-              url: `${APP_BASE_URL}/${locale}/results/${year}/${category}/${driver.driverId}`,
-              lastModified: new Date(),
-              changeFrequency: "weekly",
-              priority: 0.8,
-            });
-          }
-        } else if (category === "constructors") {
-          const constructors = await getAllConstructors(year);
-          for (const constructor of constructors) {
-            // constructor specific pages
-            routes.push({
-              url: `${APP_BASE_URL}/${locale}/results/${year}/${category}/${constructor.constructorId}`,
-              lastModified: new Date(),
-              changeFrequency: "weekly",
-              priority: 0.8,
-            });
-          }
-        } else if (category === "races") {
-          const racesData = await getAllRaces(year);
-          const races = racesData?.MRData.RaceTable.Races ?? [];
-          for (const race of races) {
-            // race specific pages
-            routes.push({
-              url: `${APP_BASE_URL}/${locale}/results/${year}/${category}/${race.Circuit.circuitId}`,
-              lastModified: new Date(),
-              changeFrequency: "weekly",
-              priority: 0.8,
-            });
-          }
-        }
       }
     }
   }

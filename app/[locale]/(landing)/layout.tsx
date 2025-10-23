703790983de2986bdef35c6f3a997e4265fa7077
tamadam <tamadam9@gmail.com>
2025-10-23 19:22:30 +0200

    Restore

--- app/[locale]/(landing)/layout.tsx
+++ app/[locale]/(landing)/layout.tsx
@@ -1,9 +1,6 @@
 import HeaderSimple from "@/app/components/Header/HeaderSimple/HeaderSimple";
+
 import { getTranslations } from "next-intl/server";
-import { getCurrentYear } from "@/app/lib/year-utils";
-import { RawRaces } from "@/app/types/races";
-import { setRequestLocale } from "next-intl/server";
-import { getAllRaces } from "@/app/lib/api/getAllRaces";
 
 export async function generateMetadata({
   params,
@@ -22,23 +19,11 @@ export async function generateMetadata({
   };
 }
 
-export default async function LandingPageLayout({
+export default function LandingPageLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
 }>) {
-  const { locale } = await params;
-
-  // Adding this fetch, same as in the sibling layout, to prevent a weird build time error
-  const currentYear: number = getCurrentYear();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const rawAllRaces: RawRaces | null = await getAllRaces(String(currentYear));
-
-  // Enable static rendering
-  setRequestLocale(locale);
-
   return (
     <>
       <HeaderSimple />

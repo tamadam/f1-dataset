3cf4f86923a587ec3b29e65ea8c53072e171f7c5
tamadam <tamadam9@gmail.com>
2025-10-23 17:37:46 +0200

    Move SessionCounter so it will be available on all pages

--- app/[locale]/(main)/layout.tsx
+++ app/[locale]/(main)/layout.tsx
@@ -1,4 +1,8 @@
 import HeaderFull from "@/app/components/Header/HeaderFull/HeaderFull";
+import NextSessionCounter from "@/app/components/NextSessionCounter/NextSessionCounter";
+import { getAllRaces } from "@/app/lib/api/getAllRaces";
+import { getCurrentYear } from "@/app/lib/year-utils";
+import { Race, RawRaces } from "@/app/types/races";
 import { setRequestLocale } from "next-intl/server";
 
 export default async function MainPageLayout({
@@ -9,6 +13,9 @@ export default async function MainPageLayout({
   params: Promise<{ locale: string }>;
 }>) {
   const { locale } = await params;
+  const currentYear: number = getCurrentYear();
+  const rawAllRaces: RawRaces | null = await getAllRaces(String(currentYear));
+  const allRacesList: Race[] = rawAllRaces?.MRData.RaceTable.Races || [];
 
   // Enable static rendering
   setRequestLocale(locale);
@@ -16,6 +23,7 @@ export default async function MainPageLayout({
   return (
     <>
       <HeaderFull />
+      <NextSessionCounter races={allRacesList} locale={locale} />
       <div>{children}</div>
     </>
   );

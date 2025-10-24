182ad83a8a8b144f3f1e274925015a4eaf2cb606
tamadam <tamadam9@gmail.com>
2025-10-24 12:22:20 +0200

    test

--- app/[locale]/(main)/layout.tsx
+++ app/[locale]/(main)/layout.tsx
@@ -1,4 +1,8 @@
 import HeaderFull from "@/app/components/Header/HeaderFull/HeaderFull";
+import NextSessionCounter from "@/app/components/NextSessionCounter/NextSessionCounter";
+import { getAllRaces } from "@/app/lib/api/getAllRaces";
+import { getCurrentYear } from "@/app/lib/year-utils";
+import { Race, RawRaces } from "@/app/types/races";
 import { setRequestLocale } from "next-intl/server";
 
 export default async function MainPageLayout({
@@ -13,9 +17,14 @@ export default async function MainPageLayout({
   // Enable static rendering
   setRequestLocale(locale);
 
+  const currentYear: number = getCurrentYear();
+  const rawAllRaces: RawRaces | null = await getAllRaces(String(currentYear));
+  const allRacesList: Race[] = rawAllRaces?.MRData.RaceTable.Races || [];
+
   return (
     <>
       <HeaderFull />
+      <NextSessionCounter races={allRacesList} locale={locale} />
       <div>{children}</div>
     </>
   );

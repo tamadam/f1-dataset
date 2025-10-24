76e2182dd629e2eb785f72f38bc015e5ff498526
tamadam <tamadam9@gmail.com>
2025-10-24 12:00:05 +0200

    generate

--- app/[locale]/(main)/results/[year]/layout.tsx
+++ app/[locale]/(main)/results/[year]/layout.tsx
@@ -1,10 +1,25 @@
 import { setRequestLocale } from "next-intl/server";
 import YearSelector from "../components/YearSelector/YearSelector";
 import styles from "./layout.module.scss";
-/* import { getCurrentYear } from "@/app/lib/year-utils";
+import { getAllF1Years, getCurrentYear } from "@/app/lib/year-utils";
+import { routing } from "@/i18n/routing";
 import { getAllRaces } from "@/app/lib/api/getAllRaces";
+import { Race, RawRaces } from "@/app/types/races";
 import NextSessionCounter from "@/app/components/NextSessionCounter/NextSessionCounter";
-import { Race, RawRaces } from "@/app/types/races"; */
+
+export const revalidate = 3600;
+export const dynamic = "force-static";
+
+export async function generateStaticParams() {
+  const historicalYears = getAllF1Years({ excludeCurrent: true });
+
+  return routing.locales.flatMap((locale) =>
+    historicalYears.flatMap((year) => ({
+      locale,
+      year: year.toString(),
+    }))
+  );
+}
 
 export default async function ResultsPageYearLayout({
   children,
@@ -15,9 +30,9 @@ export default async function ResultsPageYearLayout({
 }>) {
   const { locale } = await params;
 
-  /*  const currentYear: number = getCurrentYear();
+  const currentYear: number = getCurrentYear();
   const rawAllRaces: RawRaces | null = await getAllRaces(String(currentYear));
-  const allRacesList: Race[] = rawAllRaces?.MRData.RaceTable.Races || []; */
+  const allRacesList: Race[] = rawAllRaces?.MRData.RaceTable.Races || [];
 
   // Enable static rendering
   setRequestLocale(locale);
@@ -25,7 +40,7 @@ export default async function ResultsPageYearLayout({
   return (
     <div className={styles.resultsPageYearLayoutWrapper}>
       <YearSelector />
-      {/*  <NextSessionCounter races={allRacesList} locale={locale} /> */}
+      <NextSessionCounter races={allRacesList} locale={locale} />
       {children}
     </div>
   );

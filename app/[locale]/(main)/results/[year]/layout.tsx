2a6878ec3ff939d17987da9b791375b25c0a26df
tamadam <tamadam9@gmail.com>
2025-10-24 11:05:34 +0200

    Add back counter

--- app/[locale]/(main)/results/[year]/layout.tsx
+++ app/[locale]/(main)/results/[year]/layout.tsx
@@ -3,8 +3,8 @@ import YearSelector from "../components/YearSelector/YearSelector";
 import styles from "./layout.module.scss";
 import { getAllRaces } from "@/app/lib/api/getAllRaces";
 
-/* import { getCurrentYear } from "@/app/lib/year-utils";
 import NextSessionCounter from "@/app/components/NextSessionCounter/NextSessionCounter";
+/* import { getCurrentYear } from "@/app/lib/year-utils";
 import { Race, RawRaces } from "@/app/types/races"; */
 
 export const revalidate = 3600;
@@ -20,9 +20,9 @@ export default async function ResultsPageYearLayout({
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const currentYear: number = 2025;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const rawAllRaces = await getAllRaces("2025");
-  /*  const allRacesList: Race[] = rawAllRaces?.MRData.RaceTable.Races || []; */
+  console.log(rawAllRaces);
+  const allRacesList = rawAllRaces?.MRData.RaceTable.Races || [];
 
   // Enable static rendering
   setRequestLocale(locale);
@@ -30,7 +30,7 @@ export default async function ResultsPageYearLayout({
   return (
     <div className={styles.resultsPageYearLayoutWrapper}>
       <YearSelector />
-      {/*  <NextSessionCounter races={allRacesList} locale={locale} /> */}
+      <NextSessionCounter races={allRacesList} locale={locale} />
       {children}
     </div>
   );

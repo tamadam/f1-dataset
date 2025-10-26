319b514edd9fc8cf90cd109d705ace5fe771f5fb
tamadam <tamadam9@gmail.com>
2025-10-26 10:08:28 +0100

    test

--- app/[locale]/Sessions.tsx
+++ app/[locale]/Sessions.tsx
@@ -2,9 +2,9 @@
 
 import { useEffect, useState } from "react";
 import { getCurrentYear } from "@/app/lib/year-utils";
-import { getAllRaces } from "@/app/lib/api/getAllRaces";
 import NextSessionCounter from "@/app/components/NextSessionCounter/NextSessionCounter";
 import { Race } from "@/app/types/races";
+import { F1_API_BASE_URL } from "../constants";
 
 interface Props {
   locale: string;
@@ -18,8 +18,9 @@ const ClientSessions = ({ locale }: Props) => {
     const fetchRaces = async () => {
       try {
         const currentYear = getCurrentYear();
-        const raw = await getAllRaces(String(currentYear));
-        setRaces(raw?.MRData?.RaceTable?.Races || []);
+        const raw = await fetch(`${F1_API_BASE_URL}/${currentYear}/races`);
+        const res = await raw.json();
+        setRaces(res?.MRData?.RaceTable?.Races || []);
       } catch (e) {
         console.error("Failed to fetch races on client:", e);
       } finally {

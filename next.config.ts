1d12efb5ef134352a9eeaab472a389699b2b96b9
tamadam <tamadam9@gmail.com>
2025-11-16 13:36:58 +0100

    cpu1

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+  experimental: {
+    cpus: 1,
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
